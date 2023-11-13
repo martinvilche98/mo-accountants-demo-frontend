eb0cae7458ed6554da693bd984af5d5a0b682c19
Martin Vilche <martinvilche98@hotmail.com>
2023-11-13 05:47:33 -0300

    It lives 🎆

---
+++ app/components/forms/login/LoginForm.tsx
@@ -0,0 +1,15 @@
+import './LoginForm.styles.css';
+
+import {Card, Grid} from '@mui/material'
+
+function LoginForm({}) {
+
+    return <>
+        <Card id="login-form-container">
+            <Grid container xs={12}>
+                Login
+            </Grid>
+        </Card>
+    </>
+}
+export default LoginForm
